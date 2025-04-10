f9fec4776904e99055540c4f78d1caf9a8b6745d
victhoriaellen <vucthoriadenyver@gmail.com>
2025-04-09 21:06:45 -0300

    Add files via upload

---
+++ aula 09 ex16.py
@@ -0,0 +1,15 @@
+amigos = ["João", "Maria", "Carlos", "Ana", "Lucas"]
+
+for amigo in amigos:
+    print(amigo)
+
+
+#while
+
+amigos = ["João", "Maria", "Carlos", "Ana", "Lucas"]
+
+i = 0
+
+while i < len(amigos):
+    print(amigos[i])
+    i += 1
