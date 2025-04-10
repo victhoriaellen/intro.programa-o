442900290a33732e2fd2959216462d4c657b24e1
victhoriaellen <vucthoriadenyver@gmail.com>
2025-04-09 21:09:37 -0300

    Add files via upload

---
+++ aula 09 ex17.py
@@ -0,0 +1,14 @@
+amigos = ["João", "Maria", "Carlos", "Ana", "Pedro"]
+
+for amigo in amigos:
+    print(f"Olá {amigo}, como vai você?")
+
+#while 
+
+amigos = ["João", "Maria", "Carlos", "Ana", "Pedro"]
+
+indice = 0
+while indice < len(amigos):
+    print(f"Olá {amigos[indice]}, como vai você?")
+    indice += 1
+
